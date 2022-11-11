422aeeac44ef6624a5e526354cf71e862e75e5c4
JakeAve <jacobjavery@gmail.com>
2022-11-10 21:28:33 -0700

    imported levenshteinDistance

---
+++ tests/utils/levenshteinDistance.test.ts
@@ -0,0 +1,10 @@
+import { levenshteinDistance } from "../../utils/levenshteinDistance.ts";
+import { assertEquals } from "https://deno.land/std@0.159.0/testing/asserts.ts";
+
+Deno.test("levenshteinDistance", () => {
+  const t1 = levenshteinDistance("fast", "faster");
+  assertEquals(t1, 2);
+
+  const t2 = levenshteinDistance("fast", "fastest");
+  assertEquals(t2, 3);
+});
---
+++ utils/levenshteinDistance.ts
@@ -0,0 +1,9 @@
+export { distance as levenshteinDistance } from "https://deno.land/x/fastest_levenshtein@1.0.10/mod.ts";
+
+// // Print levenshtein-distance between 'fast' and 'faster'
+// console.log(distance("fast", "faster"));
+// //=> 2
+
+// // Print string from array with lowest edit-distance to 'fast'
+// console.log(closest("fast", ["slow", "faster", "fastest"]));
+// //=> 'faster'
